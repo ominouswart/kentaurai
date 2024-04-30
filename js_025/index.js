17c11afe402c86902ba56effe1870bb90cda9f56
Timas Stankevicius <timothystankus444@gmail.com>
2024-04-30 15:42:06 +0300

    bbz

--- js_025/index.js
+++ js_025/index.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 80;
+app.use(express.static('public'));
 
 // Router
-
-app.get('/', (req, res) => {
-    const content = '<h1>Bla bla</h1>';
-  res.send(content);
-});
 
 app.get('/bebras', (req, res) => {
     const content = '<h1>Labas bebrai</h1>';
@@ -27,6 +23,58 @@ app.get('/zuikis', (req, res) => {
 app.get('/zveris/:animal', (req, res) => {
     const animal = req.params.animal;
     const content = '<h1>Labas, ' + animal + '</h1>';
+  res.send(content);
+});
+
+app.get('/sum/:firstNumber/:secondNumber', (req, res) => {
+  const first = parseInt(req.params.firstNumber);
+  const second = parseInt(req.params.secondNumber);
+  const sum = first + second;
+  const content = '<h1>Suma: ' + sum + '</h1>';
+res.send(content);
+});
+
+app.get('/sum/:firstNumber/plus/:secondNumber', (req, res) => {
+  const first = parseInt(req.params.firstNumber);
+  const second = parseInt(req.params.secondNumber);
+  const sum = first + second;
+  const content = '<h1>Suma: ' + sum + '</h1>';
+res.send(content);
+});
+
+app.get('/sum/calc/:firstNumber/:what/:secondNumber', (req, res) => {
+  const what = req.params.what;
+
+  const first = parseInt(req.params.firstNumber);
+  const second = parseInt(req.params.secondNumber);
+  let sum = 0;
+  if (what === 'plus') {
+    sum = first + second;
+  } else if (what === 'minus') {
+    sum = first - second;
+  }
+  const content = '<h1>Suma: ' + sum + '</h1>';
+res.send(content);
+});
+
+// QUERY QUERY QUERY QUERY QUERY QUERY
+
+// http://localhost/narve?sedi=karve&guli=zuikis
+
+app.get('/narve', (req, res) => {
+  const kas = req.query.sedi;
+  const kas2 = req.query.guli;
+
+  const content = '<h1>Sedi: ' + kas + '</h1><h1>Guli: ' + kas2 + '</h1>';
+  res.send(content);
+});
+
+app.get('/sumatorius', (req, res) => {
+  const kas = parseInt(req.query.num);
+  const kas2 = parseInt(req.query.num2);
+  const sum = kas + kas2;
+
+  const content = '<h1>Sedi: ' + sum + '</h1>';
   res.send(content);
 });
 

2cd84f50c3579ae669f83c8eaaabf20534df6a61
Timas Stankevicius <timothystankus444@gmail.com>
2024-05-07 15:30:10 +0300

    animals

---
+++ js_028/index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { v4: uuidv4 } = require('uuid');
+const app = express();
+const port = 80;
+
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+const fs = require('node:fs');
+
+app.post('/add-animal', (req, res) => {
+    const animal = req.body.animal;
+    const id = uuidv4();
+
+    let data = fs.readFileSync('./data/animals.json', 'utf8');
+    data = JSON.parse(data);
+
+    data.push({id, animal});
+
+    data = JSON.stringify(data);
+
+    fs.writeFileSync('./data/animals.json', data);
+
+    res.json({
+        message: 'OK'
+    });
+});
+
+app.get('/get-animals', (req, res) => {
+
+    let data = fs.readFileSync('./data/animals.json', 'utf8');
+
+
+    data = JSON.parse(data);
+
+    const sort = req.query.sort;
+
+    if (sort === 'asc') {
+        data.sort((a, b) => a.animal.localeCompare(b.animal));
+    } else if (sort === 'desc') {
+        data.sort((a, b) => b.animal.localeCompare(a.animal));
+    }
+
+    res.json ({
+        message: 'OK',
+        animals: data
+    });
+});
+
+app.get('/show-animal/:uuid', (req, res) => {
+
+    let html = fs.readFileSync('./data/animals.html', 'utf8');
+
+    let data = fs.readFileSync('./data/animals.json', 'utf8');
+
+    data = JSON.parse(data);
+
+    const find = data.find(a => a.id === req.params.uuid);
+
+    html = html.replace('{{ANIMAL}}', find.animal);
+
+    res.send(html);
+});
+
+
+app.listen(port, () => {
+    console.log(`js_028 app listening on port ${port}`);
+  });
---
+++ js_028/src/app.js
@@ -0,0 +1,59 @@
+import axios from 'axios';
+import { error } from 'laravel-mix/src/Log';
+
+window.addEventListener('load', _ => {
+    const input = document.querySelector('input');
+    const button = document.querySelector('button');
+
+    button.addEventListener('click', _ => {
+
+        const animal = input.value;
+
+        input.value = '';
+
+        axios.post('http://localhost/add-animal', {animal})
+        .then(res => {
+            console.log(res.data);
+            getAllAnimals();
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    });
+
+    document.querySelector('.a-z').addEventListener('click', _ => {
+        getAllAnimals('asc')
+    });
+
+    document.querySelector('.z-a').addEventListener('click', _ => {
+        getAllAnimals('desc')
+    });
+
+    const getAllAnimals = (sort = '') => {
+        const ul = document.querySelector('ul');
+        axios.get('http://localhost/get-animals?sort=' + sort)
+        .then(res => {
+            let html = '';
+            res.data.animals.forEach(a => {
+                html += `
+                <li>
+                    <span>${a.animal}</span>
+                    <a href="http://localhost/show-animal/${a.id}">Show</a>
+                </li>
+                `;
+            });
+            ul.innerHTML = html;
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+
+
+
+
+    getAllAnimals();
+
+
+
+});

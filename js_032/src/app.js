2433b446a3e183e4ae5e9e67677144697f25bc80
Timas Stankevicius <timothystankus444@gmail.com>
2024-05-15 15:57:14 +0300

    colors

--- js_032/src/app.js
+++ js_032/src/app.js
@@ -1,1 +1,16 @@
 import 'bootstrap';
+
+
+
+window.addEventListener('load', _ => {
+
+    if (document.querySelector('.with-csr')) {
+
+        const checkedInput = document.querySelector(`#shape${shape}`);
+
+        checkedInput.checked = true;
+    }
+
+
+
+});

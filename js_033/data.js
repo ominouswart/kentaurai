ab488bee8eedf7245c86dcedc5eec822f34f8ee8
Timas Stankevicius <timothystankus444@gmail.com>
2024-05-21 21:43:06 +0300

    zoo

--- js_033/data.js
+++ js_033/data.js
@@ -1,3 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 80;
+
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+const fs = require('node:fs');
+
 const animalZoo = [
     {name: 'Lion', species: 'Panthera leo', age: 10},
     {name: 'Tiger', species: 'Panthera tigris', age: 12},
@@ -15,6 +26,43 @@ const animalZoo = [
     {name: 'Wallaby', species: 'Macropus', age: 5},
     {name: 'Dingo', species: 'Canis lupus dingo', age: 8}
 ];
- 
-// Sukurti serverį ir serveryje SSR būdu sugeneruoti žvėris. Žvėris atspausdinti naršyklėje.
-// (žvėris į json failą sudėkite naudodami seeder'į)
+
+
+
+
+const getAnimals = () => {
+    let data = fs.readFileSync('./data/animals.json', 'utf8');
+    return JSON.parse(data);
+};
+
+
+const addAnimalAndDisplay = (name, species, age) => {
+    const animals = getAnimals();
+    const newAnimal = { name, species, age };
+    animals.push(newAnimal);
+
+    return animals;
+};
+
+
+app.get('/', (req, res) => {
+    fs.writeFileSync('./data/animals.json', JSON.stringify(animalZoo));
+
+    let html = fs.readFileSync('./data/index.html', 'utf8');
+    const listItem = fs.readFileSync('./data/listItem.html', 'utf8');
+    let data = fs.readFileSync('./data/animals.json', 'utf8');
+    data = JSON.parse(data);
+    let listItems = '';
+    data.forEach(li => {
+        let liHtml = listItem;
+        liHtml = liHtml.replace('{{NAME}}', li.name).replace('{{SPECIES}}', li.species).replace('{{AGE}}', li.age);
+        listItems += liHtml;
+    });
+    html = html.replace('{{LI}}', listItems);
+    res.send(html);
+});
+
+
+app.listen(port, _ => {
+    console.log(`animals app listening on port ${port}`);
+});

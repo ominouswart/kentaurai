a9367149040579061fbab2c10f4972ceca170349
Timas Stankevicius <timothystankus444@gmail.com>
2024-05-28 15:31:56 +0300

    join

--- db/app.js
+++ db/app.js
@@ -20,6 +20,39 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 
+app.get('/book', (_, res) => {
+
+    let html = fs.readFileSync('./data/book.html', 'utf8');
+    const listItem = fs.readFileSync('./data/listItem2.html', 'utf8');
+
+    const sql = `
+    SELECT c.id, name, number
+    FROM clients AS c
+    INNER JOIN numbers AS n 
+    ON c.id = n.client_id
+    `;
+
+
+    connection.query(sql, (err, rows) => {
+        if (err) throw err;
+      
+        console.log('Answer: ', rows);
+        
+        let listItems = '';
+        rows.forEach(client => {
+            let liHtml = listItem;
+            liHtml = liHtml.replace('{{ID}}', client.id)
+            .replace('{{NAME}}', client.name)
+            .replace('{{PHONE}}', client.number)
+            listItems += liHtml;
+        });
+        html = html.replace('{{LI}}', listItems);
+        res.send(html);
+      });
+
+});
+
+
 app.get('/', (_, res) => {
 
     let html = fs.readFileSync('./data/index.html', 'utf8');
@@ -54,7 +87,14 @@ app.get('/', (_, res) => {
 
 app.post('/plant', (req, res) => {
 
-    const { name, height, type } = req.body;
+    let { name, height, type } = req.body;
+
+    // mini sanitization
+
+    height = parseFloat(height);
+    isNaN(height) && (height = 0);
+    !['lapuotis', 'spygliutos', 'palme'].includes(type) && (type = 'lapuotis');
+    !(name) && (name = '---');
 
     // const sql = `
     // INSERT INTO trees (name, height, type)
@@ -67,7 +107,7 @@ app.post('/plant', (req, res) => {
     `;
 
 
-    connection.query(sql, [name, parseFloat(height), type], (err) => {
+    connection.query(sql, [name, height, type], (err) => {
         if (err) throw err;
         res.redirect(302, 'http://localhost:8080/');
     });
@@ -114,6 +154,69 @@ app.post('/water', (req, res) => {
     });
 
 });
+
+app.get('/stats', (_, res) => {
+
+    let html = fs.readFileSync('./data/stats.html', 'utf8');
+
+    const sql = `
+    SELECT MIN(height) AS min, MAX(height) AS max, COUNT(*) AS count, SUM(height) AS sum, AVG(height) AS avg
+    FROM trees
+    `;
+
+
+    connection.query(sql, (err, rows) => {
+        if (err) throw err;
+
+
+        html = html.replace('{{MAX}}', rows[0].max)
+        .replace('{{MIN}}', rows[0].min)
+        .replace('{{SUM}}', rows[0].sum)
+        .replace('{{COUNT}}', rows[0].count)
+        .replace('{{AVG}}', rows[0].avg);
+
+        res.send(html);
+    });
+
+
+
+    
+    
+});
+
+app.get('/find', (req, res) => {
+
+    let html = fs.readFileSync('./data/find.html', 'utf8');
+    const listItem = fs.readFileSync('./data/listItem.html', 'utf8');
+
+
+    let s = req.query.s;
+
+    const sql = `
+    SELECT id, name, height, type
+    FROM trees
+    -- WHERE name LIKE ?
+    `;
+
+
+    connection.query(sql, ['%' + s + '%'], (err, rows) => {
+        if (err) throw err;
+        
+        let listItems = '';
+        rows.forEach(tree => {
+            let liHtml = listItem;
+            liHtml = liHtml.replace('{{ID}}', tree.id)
+            .replace('{{NAME}}', tree.name)
+            .replace('{{HEIGHT}}', tree.height)
+            .replace('{{TYPE}}', tree.type);
+            listItems += liHtml;
+        });
+        html = html.replace('{{LI}}', listItems).replace('{{S}}', s);
+        res.send(html);
+      });
+});
+
+
 
 
 app.listen(port, _ => {

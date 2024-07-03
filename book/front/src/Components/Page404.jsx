7aa3ce6c870e50bff4c9e2e9352541c88c0e9713
Timas Stankevicius <timothystankus444@gmail.com>
2024-07-03 18:21:51 +0300

    lastproj

---
+++ book/front/src/Components/Page404.jsx
@@ -0,0 +1,17 @@
+export default function Page404() {
+    return (
+        <div id="wrapper">
+            <div id="main">
+                <div className="inner">
+                    <section>
+                        <header className="main">
+                            <h1>404 Page not found</h1>
+                            <div id="header"></div>
+                            <a href="/#">Back to Home</a>
+                        </header>
+                    </section>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ book/front/src/Contexts/Router.jsx
@@ -0,0 +1,81 @@
+import { createContext, useCallback, useEffect, useState } from "react";
+import Page404 from "../Components/Page404";
+
+
+const RouterCountext = createContext([]);
+
+
+const Home = _ => {
+    return <div>
+        <h1>HOME</h1>
+        <a href="/#bebras/22">Bebras</a>
+        <a href="/#zebras/22">Zebras</a>
+        <a href="/#">Home</a>
+    </div>
+}
+
+const Bebras = _ => {
+    return <div>
+        <h1>BEBRAS</h1>
+        <a href="/#bebras/22">Bebras</a>
+        <a href="/#zebras/22">Zebras</a>
+        <a href="/#">Home</a>
+    </div>
+}
+
+const Zebras = _ => {
+    return <div>
+        <h1>ZEBRAS</h1>
+        <a href="/#bebras/22">Bebras</a>
+        <a href="/#zebras/22">Zebras</a>
+        <a href="/#">Home</a>
+    </div>
+}
+
+
+
+const Router = _ => {
+
+
+    const [route, setRoute] = useState('');
+    const [params, setParams] = useState([]);
+
+    const handleHashChange = useCallback(_ => {
+        const hash = window.location.hash.split('/');
+        hash[0] || (hash[0] = '#');
+        setRoute(hash.shift());
+        setParams(hash);
+    }, [setRoute, setParams])
+
+    useEffect(_ => {
+        const hash = window.location.hash.split('/');
+        hash[0] || (hash[0] = '#');
+        setRoute(hash.shift());
+        setParams(hash);
+
+        window.addEventListener('hashchange', handleHashChange);
+
+        return _ => window.removeEventListener('hashchange', handleHashChange);
+
+    }, [handleHashChange]);
+
+    const routes = [
+        { path: '', pc: 0, component: null },
+        { path: '#', pc: 0, component: <Home /> },
+        { path: '#bebras', pc: 1, component: <Bebras /> },
+        { path: '#zebras', pc: 1, component: <Zebras /> }
+    ];
+
+    const routeComponent = routes.find(r => r.path === route && r.pc === params.length)?.component ?? <Page404 />
+
+
+    return (
+        <RouterCountext.Provider value={params} >
+            {routeComponent}
+        </RouterCountext.Provider>
+    )
+}
+
+export {
+    RouterCountext, Router
+}
